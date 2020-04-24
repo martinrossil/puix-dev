3f33e26196a0fb212931312eba1f18d10ed6ce5c
Martin Rossil <martinrossil@gmail.com>
2020-04-24 12:41:17 +0200

    Updated.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,9 +1,9 @@
-import { greet } from 'puix';
-export default class PuixDev extends HTMLElement {
+import { DisplayElement } from 'puix';
+export default class PuixDev extends DisplayElement {
     constructor() {
         super();
         console.log('PuixDev constructor()');
-        greet();
+        this.dispatchEventWith('test', this)
     }
 }
 customElements.define('puix-dev', PuixDev);
