d271ae352e48358df4f19e5efc5942da30066835
Martin Rossil <martinrossil@gmail.com>
2020-04-29 14:50:25 +0200

    Updated puix.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,8 +1,18 @@
-import { DisplayElement } from 'puix';
-export default class PuixDev extends DisplayElement {
+import { DisplayElement, IDisplayElement, DisplayContainer } from 'puix';
+export default class PuixDev extends DisplayContainer {
     constructor() {
         super();
         console.log('PuixDev', 'super()');
+        console.log(this.name, 'super()');
+        this.backgroundColor = 'red';
+        const d: IDisplayElement = new DisplayElement();
+        d.setSize(300, 50);
+        d.backgroundColor = 'green';
+        this.addElement(d);
+        const d2: IDisplayElement = new DisplayElement();
+        d2.setSize(50, 300);
+        d2.backgroundColor = 'blue';
+        this.addElement(d2);
     }
 }
 customElements.define('puix-dev', PuixDev);
