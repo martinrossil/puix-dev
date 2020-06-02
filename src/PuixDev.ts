5fcec405f03f6589b8be09e45201442bf1b20e3f
Martin Rossil <martinrossil@gmail.com>
2020-06-02 15:40:51 +0200

    Added svg and filters.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,4 +1,4 @@
-import { ApplicationElement, AnchorLayout, AppBar, DataCard } from 'puix';
+import { ApplicationElement, AnchorLayout, IShapeElement, ShapeElement, AnchorLayoutData, IShadowFilter, ShadowFilter, HSL, CornerType } from 'puix';
 
 export default class PuixDev extends ApplicationElement {
     constructor() {
@@ -6,8 +6,15 @@ export default class PuixDev extends ApplicationElement {
         this.name = 'PuixDev';
         this.backgroundColor = this.theme.colors.neutral.c50;
         this.layout = new AnchorLayout();
-        this.addElement(new AppBar());
-        this.addElement(new DataCard());
+        const shp: IShapeElement = new ShapeElement();
+        shp.setSize(150, 150);
+        shp.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
+        shp.fillColor = HSL.CYAN_500;
+        shp.cornerSize = 75;
+        shp.cornerType = CornerType.CUT;
+        const sf: IShadowFilter = new ShadowFilter(0, 10, 10, HSL.CYAN_900, 0.3);
+        shp.filter = sf;
+        this.addElement(shp);
     }
 }
 customElements.define('puix-dev', PuixDev);
