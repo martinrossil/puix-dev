310a6f9495abe733737672d7d5c8bdfc8f4b18e6
Martin Rossil <martinrossil@gmail.com>
2020-06-16 00:23:54 +0200

    Inlined fonts.css and polyfill.js

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -6,15 +6,17 @@ export default class PuixDev extends ApplicationElement {
         this.name = 'PuixDev';
         this.backgroundColor = this.theme.colors.neutral.c50;
         this.layout = new AnchorLayout();
-        const shp: IShapeElement = new ShapeElement();
-        shp.setSize(150, 150);
-        shp.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
-        shp.fillColor = HSL.CYAN_500;
-        shp.cornerSize = 75;
-        shp.cornerType = CornerType.CUT;
-        const sf: IShadowFilter = new ShadowFilter(0, 10, 10, HSL.CYAN_900, 0.3);
-        shp.filter = sf;
-        this.addElement(shp);
+        this.shp.setSize(150, 150);
+        this.shp.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
+        this.shp.fillColor = HSL.CYAN_500;
+        this.shp.cornerSize = 75;
+        this.shp.cornerType = CornerType.CUT;
+        this.shp.filter = this.sf;
+        this.addElement(this.shp);
     }
+
+    private sf: IShadowFilter = new ShadowFilter(0, 10, 10, HSL.CYAN_900, 0.3);
+
+    private shp: IShapeElement = new ShapeElement();
 }
 customElements.define('puix-dev', PuixDev);
