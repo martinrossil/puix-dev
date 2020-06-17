026b438a72f4db9d628a62ee1d1f511e12e45639
Martin Rossil <martinrossil@gmail.com>
2020-06-17 23:38:27 +0200

    Updated app.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,22 +1,21 @@
-import { ApplicationElement, AnchorLayout, IShapeElement, ShapeElement, AnchorLayoutData, IShadowFilter, ShadowFilter, HSL, CornerType } from 'puix';
+import { ApplicationElement, AnchorLayoutData, RippleComponent } from 'puix';
 
 export default class PuixDev extends ApplicationElement {
     constructor() {
         super();
         this.name = 'PuixDev';
-        this.backgroundColor = this.theme.colors.neutral.c50;
-        this.layout = new AnchorLayout();
-        this.shp.setSize(150, 150);
-        this.shp.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
-        this.shp.fillColor = HSL.CYAN_500;
-        this.shp.cornerSize = 75;
-        this.shp.cornerType = CornerType.CUT;
-        this.shp.filter = this.sf;
-        this.addElement(this.shp);
+        this.addElement(this.rc);
     }
 
-    private sf: IShadowFilter = new ShadowFilter(0, 10, 10, HSL.CYAN_900, 0.3);
+    private _rc: RippleComponent | null = null;
 
-    private shp: IShapeElement = new ShapeElement();
+    protected get rc(): RippleComponent {
+        if (!this._rc) {
+            this._rc = new RippleComponent();
+            this._rc.setSize(200, 200);
+            this._rc.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
+        }
+        return this._rc;
+    }
 }
 customElements.define('puix-dev', PuixDev);
