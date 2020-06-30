0f53a92683b17b1163ff62505a5fc0aca0847e1b
Martin Rossil <martinrossil@gmail.com>
2020-06-30 23:24:40 +0200

    Updated package.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,21 +1,10 @@
-import { ApplicationElement, AnchorLayoutData, RippleComponent } from 'puix';
+import { ApplicationElement, AppBar } from 'puix';
 
 export default class PuixDev extends ApplicationElement {
     constructor() {
         super();
         this.name = 'PuixDev';
-        this.addElement(this.rc);
-    }
-
-    private _rc: RippleComponent | null = null;
-
-    protected get rc(): RippleComponent {
-        if (!this._rc) {
-            this._rc = new RippleComponent();
-            this._rc.setSize(200, 200);
-            this._rc.layoutData = new AnchorLayoutData(NaN, NaN, NaN, NaN, 0, 0);
-        }
-        return this._rc;
+        this.addElement(new AppBar());
     }
 }
 customElements.define('puix-dev', PuixDev);
