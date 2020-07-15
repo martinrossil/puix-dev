b1b429d480dbddc887ad520bcb2acd90c8795b9e
Martin Rossil <martinrossil@gmail.com>
2020-07-15 23:07:37 +0200

    Updated dependencies.

--- src/PuixDev.ts
+++ src/PuixDev.ts
@@ -1,10 +1,11 @@
-import { ApplicationElement, AppBar } from 'puix';
+import { ApplicationElement, AppBar, DataCard } from 'puix';
 
 export default class PuixDev extends ApplicationElement {
     constructor() {
         super();
         this.name = 'PuixDev';
         this.addElement(new AppBar());
+        this.addElement(new DataCard());
     }
 }
 customElements.define('puix-dev', PuixDev);
